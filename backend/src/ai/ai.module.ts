eb766454296dc2555412ec336ed0949aacb746a0
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-09-02 22:46:44 -0300

    refactor ai folder

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common"
 import { AiController } from "./ai.controller";
 import { AiService } from "./ai.service";
-import { ConfigModule } from "@nestjs/config";
 
 @Module({
     controllers: [AiController],
